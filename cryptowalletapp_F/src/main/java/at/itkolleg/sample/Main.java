252aa87545d1f0de3f916876c7c6c6e502da0544
meos1406 <meos1406@gmail.com>
2021-05-04 17:43:02 +0200

    project status F
    Startprojekt für Video "G Wallet Klasse Teil 3"

---
+++ cryptowalletapp_F/src/main/java/at/itkolleg/sample/Main.java
@@ -0,0 +1,76 @@
+package at.itkolleg.sample;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ResourceBundle;
+
+import domain.BankAccount;
+import domain.CryptoCurrency;
+import domain.Transaction;
+import domain.Wallet;
+import exceptions.InsufficientBalanceException;
+import exceptions.InvalidAmountException;
+import exceptions.InvalidFeeException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        AnchorPane root = FXMLLoader.load(Main.class.getResource("main.fxml"),
+                ResourceBundle.getBundle("at.itkolleg.sample.main"));
+
+        Scene scene = new Scene(root, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt");
+
+        BankAccount ba = new BankAccount();
+        ba.deposit(new BigDecimal("10000"));
+
+        System.out.println(ba);
+
+        CryptoCurrency crypto = CryptoCurrency.BTC;
+        System.out.println(crypto.getCurrencyName());
+        System.out.println(crypto.getCode());
+        System.out.println(CryptoCurrency.valueOfCode("BTC").getCurrencyName());
+
+        Transaction transaction = new Transaction(
+                CryptoCurrency.ETH,
+                new BigDecimal("1.23"),
+                new BigDecimal("1567.8")
+        );
+
+        System.out.println(transaction);
+
+        Wallet wallet = null;
+        try {
+            wallet = new Wallet("My BTC Wallet", CryptoCurrency.BTC, new BigDecimal("0.01"));
+        } catch (InvalidFeeException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            wallet.buy(new BigDecimal("100"), new BigDecimal("10"), ba);
+        } catch (InvalidAmountException e) {
+            e.printStackTrace();
+        } catch (InsufficientBalanceException insufficientBalanceException) {
+            insufficientBalanceException.printStackTrace();
+        }
+
+        System.out.println(ba);
+
+        System.out.println(wallet);
+
+
+        launch(args);
+    }
+}
---
+++ cryptowalletapp_F/src/main/java/domain/Wallet.java
@@ -0,0 +1,91 @@
+package domain;
+
+import exceptions.InsufficientBalanceException;
+import exceptions.InvalidAmountException;
+import exceptions.InvalidFeeException;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Wallet implements Serializable {
+    private final UUID id;
+    private final String name;
+    private final CryptoCurrency cryptoCurrency;
+    private BigDecimal amount;
+    private final List<Transaction> transactions;
+    private BigDecimal feeInPercent;
+
+    public Wallet(String name, CryptoCurrency cryptoCurrency, BigDecimal feeInPercent) throws InvalidFeeException {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.cryptoCurrency = cryptoCurrency;
+        this.amount = new BigDecimal("0");
+        this.transactions = new ArrayList<>();
+        this.setNewFee(feeInPercent);
+    }
+
+    public void buy(BigDecimal amount, BigDecimal currentPrice, BankAccount bankAccount) throws InvalidAmountException, InsufficientBalanceException {
+        if (amount.compareTo(new BigDecimal("0")) <= 0) {
+            throw new InvalidAmountException();
+        }
+        Transaction transaction = new Transaction(this.cryptoCurrency, amount, currentPrice.setScale(6, RoundingMode.HALF_UP));
+        bankAccount.withdraw(
+                transaction.getTotal().multiply
+                        (new BigDecimal("100").add(this.feeInPercent).divide(new BigDecimal("100"))
+                                .setScale(6, RoundingMode.HALF_UP)));
+        this.transactions.add(transaction);
+        this.amount = this.amount.add(transaction.getAmount());
+    }
+
+    public void setNewFee(BigDecimal fee) throws InvalidFeeException {
+        if (fee.compareTo(new BigDecimal("0")) >= 0) {
+            this.feeInPercent = fee;
+        } else {
+            throw new InvalidFeeException();
+        }
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CryptoCurrency getCryptoCurrency() {
+        return cryptoCurrency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public List<Transaction> getTransactions() {
+        return List.copyOf(transactions);
+    }
+
+    public BigDecimal getFeeInPercent() {
+        return feeInPercent;
+    }
+
+    public String getCurrencyName() {
+        return this.cryptoCurrency.getCurrencyName();
+    }
+
+    @Override
+    public String toString() {
+        return "Wallet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cryptoCurrency=" + cryptoCurrency +
+                ", amount=" + amount +
+                ", transactions=" + transactions +
+                ", feeInPercent=" + feeInPercent +
+                '}';
+    }
+}
