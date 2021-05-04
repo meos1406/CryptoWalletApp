a5f9bc0aed5a03105d96acfd53f645514690d260
meos1406 <meos1406@gmail.com>
2021-05-04 17:13:08 +0200

    project status C
    Startprojekt für Video "D CryptoCurrencyEnum und Transaction Klasse"

---
+++ cryptowalletapp_C/src/main/java/at/itkolleg/sample/Main.java
@@ -0,0 +1,44 @@
+package at.itkolleg.sample;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ResourceBundle;
+
+import domain.BankAccount;
+import exceptions.InsufficientBalanceException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        AnchorPane root = FXMLLoader.load(Main.class.getResource("main.fxml"),
+                ResourceBundle.getBundle("at.itkolleg.sample.main"));
+
+        Scene scene = new Scene(root, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt");
+        BankAccount ba = new BankAccount();
+        ba.deposit(new BigDecimal("100"));
+        System.out.println(ba);
+
+        try {
+            ba.withdraw(new BigDecimal("50"));
+            ba.withdraw(new BigDecimal("51"));
+            System.out.println(ba);
+        } catch (InsufficientBalanceException insufficientBalanceException) {
+            //System.out.println("Zu wenig Geld am Konto!");
+            System.out.println(insufficientBalanceException.getMessage());
+        }
+
+        launch(args);
+    }
+}
